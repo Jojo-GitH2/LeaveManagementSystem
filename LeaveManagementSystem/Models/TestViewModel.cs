e08827dd5e7552351f5b4bb6d3a708307c90de11
Jonah Uka <jonah.uka@gmail.com>
2025-02-26 00:23:39 +0100

    Changed nav background to dark

---
+++ LeaveManagementSystem/Models/TestViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace LeaveManagementSystem.Models;
+
+public class TestViewModel
+{
+
+    public string? Name { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public string? Address { get; set; }
+    public string? City { get; set; }
+}
